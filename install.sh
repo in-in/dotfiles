4e11406ea21c1cd3a18ccbaa07b2fc45401be50e
in-in <0866222@gmail.com>
2017-08-29 23:38:24 +0300

    update

--- install.sh
+++ install.sh
@@ -17,15 +17,19 @@ sudo apt-get install -y "${apps[@]}"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-printf "Looking for an existing zsh config...\n"
-  if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
-    printf "Found ~/.zshrc. Backing up to ~/.zshrc.old\n";
-    mv ~/.zshrc ~/.zshrc.old;
-  fi
+echo '***'
+echo 'END'
+echo '***'
 
-git clone https://github.com/in-in/dotfiles.git
-
-ln -sv "$DOTFILES_DIRECTORY/shell/.zshrc" $HOME
+# printf "Looking for an existing zsh config...\n"
+#   if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
+#     printf "Found ~/.zshrc. Backing up to ~/.zshrc.old\n";
+#     mv ~/.zshrc ~/.zshrc.old;
+#   fi
+# 
+# git clone https://github.com/in-in/dotfiles.git
+# 
+# ln -sv "$DOTFILES_DIRECTORY/shell/.zshrc" $HOME
 
 # shutdown -r now
 
