abd33c1ab123ae048ccc8b6a66a143a866c3edec
in-in <0866222@gmail.com>
2017-12-03 10:13:25 +0300

    update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,11 +1,48 @@
+(require 'package)
+
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+
 (package-initialize)
 
 (tool-bar-mode -1) ;; hide toolbar
+(menu-bar-mode -1) ;; hide menu-bar
 
+;; disable backup/autosave files
 (setq make-backup-files nil) ;; don't want any backup files
 (setq auto-save-list-file-name nil) ;; don't want any .saves files
 (setq auto-save-default nil) ;; don't want any auto saving
 
+(setq ring-bell-function 'ignore) ;; turn off sound signal
+
 (load-theme 'tsdh-dark t) ;; load theme
 
-(set-default-font "Hack-11") ;; set font
+;; Start window size
+(when window-system (set-frame-size (selected-frame) 130 40))
+(add-to-list 'default-frame-alist '(width . 130))
+(add-to-list 'default-frame-alist '(height . 40))
+
+(set-default-font "Hack-14") ;; set font
+(size-indication-mode t) ;; file size in mode-line
+
+
+;; Inhibit startup/splash screen
+(setq inhibit-splash-screen t)
+(setq ingibit-startup-message t)
+
+;; End of file newlines
+(setq require-final-newline t)
+
+;; Enable Ido Everywhere
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+;; Enable convert the region to upper case and to lower case
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+;; Load dired-x
+(require 'dired-x)
