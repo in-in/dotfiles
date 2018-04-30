0f06eea197f067fcd447f37d3af7190ec8f30b61
in-in <0866222@gmail.com>
2018-04-30 23:33:49 +0300

    combine all utility tasks in the one task

--- install.sh
+++ install.sh
@@ -5,27 +5,23 @@ sudo apt-add-repository -y ppa:ansible/ansible &&
 sudo apt-get update &&
 sudo apt-get install -y python-apt python-jmespath ansible git &&
 
-DOTFILES_DIRECTORY=$HOME/dotfiles
+DOTFILES_DIRECTORY=$HOME/dots
 
-git clone https://github.com/in-in/dotfiles.git "$DOTFILES_DIRECTORY"
+git clone https://dmin@bitbucket.org/dmin/dots.git "$DOTFILES_DIRECTORY"
 
 cd $DOTFILES_DIRECTORY
 
 ansible-playbook --ask-become-pass --verbose --vault-id @prompt playbook.yml
 
-#https://github.com/sloria/dotfiles
-#https://github.com/wincent/wincent
-#https://github.com/ansible/ansible/blob/devel/examples/ansible.cfg
-
-
-# ansible localhost -m setup
 # ansible localhost -m command -a "uptime"
-# ansible-playbook --syntax-check playbook.yml
+# ansible localhost -m setup
+# ansible-doc -l | grep apt
+# ansible-galaxy init zsh
+# ansible-galaxy init zsh --offline
+# ansible-playbook --ask-become-pass playbook.yml
 # ansible-playbook --check playbook.yml
+# ansible-playbook --syntax-check playbook.yml
 # ansible-playbook -Kv --check playbook.yml
 # ansible-playbook -Kv --vault-id @prompt playbook.yml
 # ansible-playbook playbook.yml --list-hosts
-# ansible-doc -l | grep apt
-# ansible-playbook --ask-become-pass playbook.yml
-# ansible-galaxy init zsh
 # ansible-vault encrypt_string --vault-id @prompt 'foobar'
