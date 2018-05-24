58c9172ed62326afe5fd9c4437fb8c40391a11cc
in-in <0866222@gmail.com>
2018-05-24 05:08:25 +0300

    improve install script and move git secret from default

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 read -s -p "SUDO Password: " pass
 echo
+read -s -p "VAULT Password: " vault_pass
+
+echo "$vault_pass" > /tmp/.vault_pass
 
 echo "$pass" | sudo -S apt remove -y update-manager &&
 sudo apt-add-repository -y ppa:ansible/ansible &&
@@ -12,9 +15,9 @@ DOTFILES_DIRECTORY=$HOME/dotfiles
 
 git clone https://github.com/in-in/dotfiles.git "$DOTFILES_DIRECTORY"
 cd $DOTFILES_DIRECTORY
-echo
 
-ansible-playbook --ask-vault-pass --verbose --extra-vars "ansible_become_pass=$pass" playbook.yml
+ansible-playbook --extra-vars "ansible_become_pass=$pass" --vault-password-file="/tmp/.vault_pass" --verbose playbook.yml
 
+# ansible-playbook --ask-become-pass --ask-vault-pass --verbose playbook.yml
 # ansible-galaxy init zsh --offline
 # ansible-vault encrypt_string 'foobar'
