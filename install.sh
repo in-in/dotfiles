b337b0254854df687b9fce2e48e6e188d58e0fa3
in-in <0866222@gmail.com>
2018-05-27 20:21:36 +0300

    add option - run without vault password

--- install.sh
+++ install.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
+common_action(){
+  echo "$pass" | sudo -S apt remove -y update-manager &&
+  sudo apt-add-repository -y ppa:ansible/ansible &&
+  sudo apt update &&
+  sudo apt install -y python-apt python-jmespath ansible git &&
+
+  DOTFILES_DIRECTORY=$HOME/dotfiles
+
+  git clone https://github.com/in-in/dotfiles.git "$DOTFILES_DIRECTORY"
+  cd $DOTFILES_DIRECTORY
+}
+
 read -s -p "SUDO Password: " pass
 echo
-read -s -p "VAULT Password: " vault_pass
-
-echo "$vault_pass" > /tmp/.vault_pass
-
-echo "$pass" | sudo -S apt remove -y update-manager &&
-sudo apt-add-repository -y ppa:ansible/ansible &&
-sudo apt update &&
-sudo apt install -y python-apt python-jmespath ansible git &&
-
-DOTFILES_DIRECTORY=$HOME/dotfiles
-
-git clone https://github.com/in-in/dotfiles.git "$DOTFILES_DIRECTORY"
-cd $DOTFILES_DIRECTORY
-
-ansible-playbook --extra-vars "ansible_become_pass=$pass" --vault-password-file="/tmp/.vault_pass" --verbose playbook.yml
+echo "Do you know the VAULT password?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+          read -s -p "VAULT Password: " vault_pass
+          echo
+          echo "$vault_pass" > /tmp/.vault_pass
+          common_action;
+          ansible-playbook --extra-vars "ansible_become_pass=$pass" --vault-password-file="/tmp/.vault_pass" --verbose playbook.yml
+          break;;
+        No )
+          common_action;
+          ansible-playbook --extra-vars "ansible_become_pass=$pass" --verbose playbook.yml
+          exit;;
+    esac
+done
 
 # ansible-playbook --ask-become-pass --ask-vault-pass --verbose playbook.yml
 # ansible-galaxy init zsh --offline
