3c7ba62490f2bd4600a349af8ee7d9f40480a0a6
in-in <0866222@gmail.com>
2018-07-07 03:51:29 +0300

    add rsnapshot

---
+++ roles/rsnapshot/files/alias.sh
@@ -0,0 +1,5 @@
+# rsnapshot: testing the configuration
+alias test_backup="rsnapshot  -c ~/.config/rsnapshot/rsnapshot.conf configtest"
+
+# rsnapshot: run backup
+alias backup="rsnapshot -V  -c ~/.config/rsnapshot/rsnapshot.conf weekly"
