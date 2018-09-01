c91bc754dc2165a88af3108e4a05c5934ee67607
in-in <0866222@gmail.com>
2018-09-01 09:02:40 +0300

    update settings

--- roles/youtube_dl/files/alias.sh
+++ roles/youtube_dl/files/alias.sh
@@ -1,13 +1,16 @@
-# TODO: is it a true way update youtube-dl?
-# youtube-dl: update
-alias upd_youtube-dl="pip install --user --upgrade youtube-dl"
+# youtube-dl: default directory
+YOUTUBE_DL_DIRECTORY="$STORAGE/video"
 
 # youtube-dl: default filename pattern
 YOUTUBE_DL_FILENAME="%(uploader)s - %(title)s.%(ext)s"
 
+# TODO: is it a true way update youtube-dl?
+# youtube-dl: update
+alias upd_youtube-dl="pip install --user --upgrade youtube-dl"
+
 # youtube-dl: default action for video
 y() {
-  youtube-dl -f "best[height <? 1080]" --output "$STORAGE/video/$YOUTUBE_DL_FILENAME" "$1"
+  youtube-dl -f "best[height <? 1080]" --output "$YOUTUBE_DL_DIRECTORY/$YOUTUBE_DL_FILENAME" "$1"
   exit
 }
 
@@ -19,7 +22,7 @@ ya() {
 
 # youtube-dl: get the full playlist
 yp() {
-  youtube-dl -f "best[height <? 1080]" --output "$STORAGE/video/%(playlist_title)s/%(uploader)s - %(playlist_index)s. %(title)s.%(ext)s" "$1"
+  youtube-dl -f "best[height <? 1080]" --output "$YOUTUBE_DL_DIRECTORY/%(playlist_title)s/%(uploader)s - %(playlist_index)s. %(title)s.%(ext)s" "$1"
   exit
 }
 
