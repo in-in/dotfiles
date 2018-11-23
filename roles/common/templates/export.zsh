5080eaf0f344b379459bc94dd59f379da87b5fbc
in-in <dmin@tuta.io>
2018-11-23 07:50:26 +0300

    common: delete line numbers for less

--- roles/common/templates/export.zsh
+++ roles/common/templates/export.zsh
@@ -2,4 +2,4 @@
 export STORAGE="{{ user_storage }}"
 
 # improve output of less
-export LESS="--tabs=2 --status-column --ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS --LINE-NUMBERS --HILITE-UNREAD"
+export LESS="--tabs=2 --status-column --ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS --HILITE-UNREAD"
