d09b9d3352da18981f9a3a7d5f76fd5eab48f71a
in-in <dmin@tuta.io>
2018-11-30 09:29:49 +0300

    common: change the default pager to most

--- roles/common/templates/export.zsh
+++ roles/common/templates/export.zsh
@@ -1,5 +1,5 @@
 # media storage
 export STORAGE="{{ user_storage }}"
 
-# improve output of less
-export LESS="--tabs=2 --status-column --ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS --HILITE-UNREAD"
+# pager with color
+export PAGER="most"
