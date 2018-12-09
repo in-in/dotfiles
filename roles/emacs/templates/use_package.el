69a82b50861694021ac4c6a5173e5a5b0b2dd1a0
in-in <dmin@tuta.io>
2018-12-09 13:28:00 +0300

    emacs: update elfeed settings

--- roles/emacs/templates/use_package.el
+++ roles/emacs/templates/use_package.el
@@ -1,4 +1,5 @@
 (setq use-package-always-ensure t)
+(setq storage-dir "~/storage/")
 
 (eval-when-compile
   (require 'use-package)
@@ -14,7 +15,7 @@
   :init
   (org-mode)
   :custom
-  (org-directory "~/storage/org")
+  (org-directory (concat storage-dir "org"))
   :bind
   ("C-c a" . org-agenda)
   ("C-c c" . org-capture)
@@ -31,9 +32,9 @@
 )
 
 (use-package flyspell
-  :config
-  (add-hook 'text-mode-hook 'flyspell-mode)
-  (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+  :hook
+  (text-mode . flyspell-mode)
+  (prog-mode . flyspell-prog-mode)
 )
 
 (use-package guess-language
@@ -41,8 +42,33 @@
 )
 
 (use-package elfeed
+  :config
+  (defun elfeed-search-format-date (date)
+    (format-time-string "%y.%m.%d %H:%M" (seconds-to-time date)))
+
+  (add-hook 'elfeed-new-entry-hook
+    (elfeed-make-tagger :feed-url "youtube\\.com"
+      :add '(youtube)))
+
+  (defun my-elfeed-tag-sort (a b)
+    (let*
+      ((a-tags
+        (format "%s" (elfeed-entry-tags a)))
+      (b-tags
+        (format "%s" (elfeed-entry-tags b))))
+      (if (string= a-tags b-tags) (< (elfeed-entry-date b)
+        (elfeed-entry-date a))) (string< a-tags b-tags)))
+
   :bind
   ("C-x w" . elfeed)
+  (:map elfeed-show-mode-map
+    ("q" . #'delete-window))
+
+  :custom
+  (elfeed-enclosure-default-dir (concat storage-dir "podcast/"))
+  (elfeed-search-sort-function #'my-elfeed-tag-sort)
+  (elfeed-search-title-min-width 80)
+  (elfeed-show-entry-switch #'pop-to-buffer)
 )
 
 (use-package elfeed-org
