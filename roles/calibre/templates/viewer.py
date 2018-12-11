15afde19a2d0b1c99134455ac3f0d44a13fba63c
in-in <dmin@tuta.io>
2018-12-11 10:12:04 +0300

    calibre: update config

--- roles/calibre/templates/viewer.py
+++ roles/calibre/templates/viewer.py
@@ -1,10 +1,16 @@
 # {{ ansible_managed }}
-remember_window_size = True
-page_flip_duration = 0.1
-top_margin = 10
-side_margin = 10
-bottom_margin = 10
-text_color = u'#111111'
 background_color = u'#e3e3e3'
-mono_family = u'{{ user_font_monospace }}'
+bottom_margin = 10
+default_font_size = 17
 minimum_font_size = 10
+mono_family = u'{{ user_font_monospace }}'
+mono_font_size = 16
+page_flip_duration = 0.1
+remember_window_size = True
+sans_family = u'{{ user_font }}'
+serif_family = u'{{ user_font_serif }}'
+show_controls = False
+side_margin = 10
+text_color = u'#111111'
+top_margin = 10
+user_css = u'body, td, th, div {\n  font-family: serif !important;\n  font-size: 1em !important;\n}\n\n:root body p {\n  font-family: serif !important;\n  font-size: 1em !important;\n}'
