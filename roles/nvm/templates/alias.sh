95afe81bcd199dd1beac5b8c5ee32460194d93b0
in-in <dmin@tuta.io>
2019-03-30 19:46:58 +0300

    feat(nvm): add new aliases

--- roles/nvm/templates/alias.sh
+++ roles/nvm/templates/alias.sh
@@ -1,2 +1,8 @@
 # fix everything in npm
-alias npmplease='rm -rf node_modules && npm cache clean --force && npm install'
+alias npmplease='rm -rf node_modules/ package-lock.json && npm cache clean --force && npm install'
+
+# npm start
+alias ns='npm run start'
+
+# show project repository url
+nsu() { npm view "$1" repository.url }
