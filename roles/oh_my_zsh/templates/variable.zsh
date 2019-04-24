8e8bff1689701d4f2fbb9bea6780514f344b563e
in-in <dmin@tuta.io>
2019-04-24 21:21:15 +0300

    feat(oh_my_zsh): disable user prompt before update

--- roles/oh_my_zsh/templates/variable.zsh
+++ roles/oh_my_zsh/templates/variable.zsh
@@ -4,5 +4,8 @@ ZSH_THEME="miloshadzic"
 # display red dots whilst waiting for completion
 COMPLETION_WAITING_DOTS="true"
 
+# automatically upgrade without prompting
+DISABLE_UPDATE_PROMPT=true
+
 # load plugins
 plugins=(git npm pip zsh-autosuggestions)
