98914e5b4764f679bb03507196ed0c02826ab825
in-in <dmin@tuta.io>
2019-06-14 05:05:04 +0300

    refactor(nvm): delete extra commands from the installation task and add an update alias

--- roles/nvm/templates/alias.sh
+++ roles/nvm/templates/alias.sh
@@ -7,6 +7,9 @@ alias ns='npm run start'
 # sort package.json based on the well-known package.json keys
 alias spj='npx sort-package-json'
 
+# update LTS release, migrate npm packages from a previous version and update npm
+alias node_update="nvm install 'lts/*' --reinstall-packages-from=default --latest-npm"
+
 # show project repository url
 nsu() { npm view "$1" repository.url; }
 
