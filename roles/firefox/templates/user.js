ef8bfb211dc027aed8c0f5b36170af079d9779dd
in-in <dmin@tuta.io>
2019-06-25 08:15:17 +0300

    fix(firefox): delete settings that stop the update process for the tor

--- roles/firefox/templates/user.js
+++ roles/firefox/templates/user.js
@@ -1,8 +1,6 @@
 user_pref("accessibility.typeaheadfind", false);
 user_pref("app.distributor.channel", "");
 user_pref("app.distributor", "");
-user_pref("app.partner.mint", "");
-user_pref("app.partner.ubuntu", "");
 user_pref("browser.bookmarks.editDialog.showForNewBookmarks", true);
 user_pref("browser.contentblocking.category", "custom");
 user_pref("browser.contentblocking.introCount", 20);
