23a2aa6bf63bd2dd1b4672e158da1ceac9a78522
in-in <dmin@tuta.io>
2019-08-07 23:32:34 +0300

    feat(mkcert): add mkcert role

---
+++ roles/mkcert/templates/export.zsh
@@ -0,0 +1,5 @@
+# ssl certificate for localhost
+export CERT_FILE_LOCALHOST="{{ mkcert_ssl_path }}/localhost.pem"
+
+# ssl key file for localhost
+export KEY_FILE_LOCALHOST="{{ mkcert_ssl_path }}/localhost-key.pem"
