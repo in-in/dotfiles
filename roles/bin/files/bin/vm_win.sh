df8d9f3ee72c636806863595c2184bbdc661f7bd
in-in <dmin@tuta.io>
2020-01-08 06:16:36 +0300

    feat(bin): add vboxmanage windows script

---
+++ roles/bin/files/bin/vm_win.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+TIMESTAMP=$(date +%s)
+OS_TYPE="Windows10_64"
+VM_NAME="${1:-${OS_TYPE}_${TIMESTAMP}}"
+VMS_PATH="$STORAGE/vms/"
+
+vboxmanage createvm --name "$VM_NAME" --ostype $OS_TYPE --register
+
+vboxmanage createmedium disk --filename "$VMS_PATH$VM_NAME/$VM_NAME.vdi" --size 20480
+
+vboxmanage storagectl "$VM_NAME" --name SATA --add SATA --controller IntelAhci
+vboxmanage storageattach "$VM_NAME" \
+  --medium "$VMS_PATH$VM_NAME/$VM_NAME.vdi" \
+  --port 0 --device 0 \
+  --storagectl SATA \
+  --type hdd
+
+vboxmanage storagectl "$VM_NAME" --name IDE --add ide
+vboxmanage storageattach "$VM_NAME" \
+  --device 0 \
+  --medium emptydrive \
+  --port 0 \
+  --storagectl IDE \
+  --type dvddrive
+
+vboxmanage sharedfolder add "$VM_NAME" \
+  --automount \
+  --hostpath "${VMS_PATH}shared" \
+  --name shared
+
+vboxmanage modifyvm "$VM_NAME" \
+  --audiocontroller hda \
+  --audioout on \
+  --boot1 disk \
+  --boot2 dvd \
+  --boot3 none \
+  --boot4 none \
+  --clipboard bidirectional \
+  --cpus 2 \
+  --draganddrop bidirectional \
+  --firmware efi \
+  --graphicscontroller vboxsvga \
+  --ioapic on \
+  --memory 4096 \
+  --rtcuseutc on \
+  --vram 256
