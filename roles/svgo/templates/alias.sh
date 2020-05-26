5afd26154298a7aa2afb1fe003e4d9533d77d482
in-in <dmin@tuta.io>
2020-05-26 23:50:42 +0300

    improvement(svgo): handle multiple files

--- roles/svgo/templates/alias.sh
+++ roles/svgo/templates/alias.sh
@@ -1,6 +1,9 @@
-# run svgo with config
+# run svgo with config and create backup origin files
 svg ()
 {
-  cp --force  --backup=numbered "$1" "$1.bac" &&
-  svgo --quiet --config={{ user_config }}/svgo/.svgo.yml "$1";
+  for file in "$@"
+  do
+    cp --force  --backup=numbered "$file" "$file.bac" &&
+    svgo --quiet --config={{ user_config }}/svgo/.svgo.yml "$file";
+  done
 }
