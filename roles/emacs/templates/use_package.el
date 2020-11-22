ea5ccc67f66dfe63a9fd3310d8a0d16844cfee48
in-in <dmin@tuta.io>
2020-11-22 21:11:23 +0300

    feat(emacs): add org-roam

--- roles/emacs/templates/use_package.el
+++ roles/emacs/templates/use_package.el
@@ -230,3 +230,16 @@
 							 nil
 							 utf-8))))
 					 (ispell-program-name (executable-find "hunspell"))))
+
+; https://github.com/org-roam/org-roam
+(use-package
+	org-roam
+	:hook (after-init . org-roam-mode)
+	:custom (org-roam-directory (concat org-directory "roam/"))
+	:bind (:map org-roam-mode-map
+							(("C-c n l" . org-roam)
+							 ("C-c n f" . org-roam-find-file)
+							 ("C-c n g" . org-roam-graph))
+							:map org-mode-map
+							(("C-c n i" . org-roam-insert))
+							(("C-c n I" . org-roam-insert-immediate))))
