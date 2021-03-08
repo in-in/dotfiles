7c202a2bd5ea11ed7b45e4085dc21e104c4f8143
in-in <dmin@tuta.io>
2021-03-08 21:36:06 +0300

    refactor(youtube_dl): add error handling

--- roles/youtube_dl/templates/alias.sh
+++ roles/youtube_dl/templates/alias.sh
@@ -15,42 +15,50 @@ alias yu="pip install --user --upgrade youtube-dl"
 
 # default action for video
 y() {
-  youtube-dl \
+  if youtube-dl \
   --no-playlist \
   -f "$YOUTUBE_DL_FILEFORMAT" \
-  --output "$YOUTUBE_DL_DIRECTORY/$YOUTUBE_DL_FILENAME" \
-  "$@"
-  exit
+  --output "$YOUTUBE_DL_DIRECTORY/$YOUTUBE_DL_FILENAME" "$@";
+  then
+    exit
+  fi
 }
 
 # download low quality video
 yl() {
-  youtube-dl \
+  if youtube-dl \
   --no-playlist \
   -f "$YOUTUBE_DL_FILEFORMAT_LQ" \
-  --output "$YOUTUBE_DL_DIRECTORY/$YOUTUBE_DL_FILENAME" \
-  "$@"
-  exit
+  --output "$YOUTUBE_DL_DIRECTORY/$YOUTUBE_DL_FILENAME" "$@";
+  then
+    exit
+  fi
 }
 
 # download only audio file
 ya() {
-  youtube-dl \
+  if youtube-dl \
   -f "140/m4a/bestaudio" \
-  --output "$STORAGE/podcast/$YOUTUBE_DL_FILENAME" "$@"
-  exit
+  --output "$STORAGE/podcast/$YOUTUBE_DL_FILENAME" "$@";
+  then
+    exit
+  fi
 }
 
 # get the full playlist
 yp() {
-  youtube-dl \
+  if youtube-dl \
   -f "$YOUTUBE_DL_FILEFORMAT" \
-  --output "$YOUTUBE_DL_DIRECTORY/%(playlist_title)s/%(playlist_index)s. %(title)s.%(ext)s" "$1"
-  exit
+  --output "$YOUTUBE_DL_DIRECTORY/%(playlist_title)s/%(playlist_index)s. %(title)s.%(ext)s" "$1";
+  then
+    exit
+  fi
 }
 
 # soundcloud action
 ysc() {
-  youtube-dl --output "$HOME/Music/sc/$YOUTUBE_DL_FILENAME" "$@"
-  exit
+  if youtube-dl --output "$HOME/Music/sc/$YOUTUBE_DL_FILENAME" "$@";
+  then
+    exit
+  fi
 }
